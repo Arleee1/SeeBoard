a83203a5caad395a5d0ed7b71824864a0e10dfd4
beakae <kantorskibrandon1@gmail.com>
2024-09-14 00:29:13 -0500

    GestureToAction class

--- backend/gestures.py
+++ backend/gestures.py
@@ -1,3 +1,32 @@
-class gesture:
-  def __init__(self, name, points):
-    
+import pyautogui
+
+class GestureToAction:
+    def __init__(self):
+        self.gestures_map = {
+            'pinch': self.mouse_click,
+            'rotate': self.swap_mode
+        }
+  
+    def map_gesture(self, gesture_name):
+        if gesture_name in self.gestures_map:
+            return self.gestures_map[gesture_name]
+        else:
+            print("Gesture not found")
+
+    def execute_gesture(self, function_name):
+        if callable(function_name):
+            function_name()
+        else:
+            print("Gesture not found")
+
+    def handle_movement(self, position):
+        screen_width, screen_height = pyautogui.size()
+        x = int(position[0] * screen_width)
+        y = int(position[1] * screen_height)
+        pyautogui.moveTo(x, y)
+
+    def mouse_click(self):
+        pyautogui.click()
+
+    def swap_mode(self):
+        pass
