793a74d52677937774f86bdefd6cba57e9e0a732
Ethan Ermovick <keg9ve@virginia.edu>
2024-09-15 05:39:13 -0400

    final updates

--- constants.py
+++ constants.py
@@ -1,10 +1,10 @@
 FRAME_RATE = 30
 global_gui_scale = 0.8
-draw_hands = True
+draw_hands = False
 print_hands = False
 
 # Constants for gestureProcessor.py
 left_bound_x, right_bound_x = .2, .8
-top_bound_y, bottom_bound_y = .3, .7
+top_bound_y, bottom_bound_y = .35, .7
 margin = 10
 velocity_x_scale, velocity_y_scale = .7, .7
--- cv/hands_reader.py
+++ cv/hands_reader.py
@@ -49,7 +49,7 @@ right_y_dampener = Dampener()
 
 
 class HandDetector:
-    def __init__(self, mode=False, maxHands=2, modelComplexity=1, detectionCon=0.5, trackCon=0.5):
+    def __init__(self, mode=False, maxHands=1, modelComplexity=1, detectionCon=0.5, trackCon=0.5):
         self.mode = mode
         self.maxHands = maxHands
         self.modelComplex = modelComplexity
