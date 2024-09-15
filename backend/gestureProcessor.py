19a6cdafbff420502370ae65f0550c5d16115f9d
Ethan Ermovick <keg9ve@virginia.edu>
2024-09-15 00:55:32 -0400

    start velocity

--- backend/gestureProcessor.py
+++ backend/gestureProcessor.py
@@ -1,9 +1,11 @@
 import mouse
+import numpy
 import pyautogui
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QCursor
 from backend.mode import Mode
 import mouse
+from numpy.linalg import norm
 
 left_bound_x, right_bound_x = .2, .8
 top_bound_y, bottom_bound_y = .2, .8
@@ -20,6 +22,9 @@ class GestureProcessor:
         self.has_changed_mode = True
         self.isMousePressed = False
         self.has_clicked = True
+        self.mouse_home_x = 0.5
+        self.mouse_home_y = 0.5
+        self.last_mouse_pos = (0.5, 0.5)
 
     def process_gesture(self, hand):
         self.handle_movement((hand['dampened_x'], hand['dampened_y']))
@@ -65,12 +70,34 @@ class GestureProcessor:
         x = (position[0] - left_bound_x) / (right_bound_x - left_bound_x) * self.screen_width
         y = (position[1] - top_bound_y) / (bottom_bound_y - top_bound_y) * self.screen_height
 
+        self.last_mouse_pos = x, y
+
         margin = 10
         if self.mode.get_mode() == "keyboard" and self.pyqt_gui:
             x = max(self.window_x + margin, min(x, self.window_x + self.screen_width) - margin)
             y = max(self.window_y + margin, min(y + self.window_y, self.window_y + self.screen_height) - margin)
-        
-        mouse.move(x, y)
+            mouse.move(x, y)
+
+        if self.mode.get_mode() == "navigation":
+            # x, y -> controller coords
+            # self.mouse_home_x, self.mouse_home_y -> mouse home
+            # output: mouse velocity vector
+
+            mouse_home = numpy.array([self.mouse_home_x, self.mouse_home_y], dtype=float)
+            controller_coords = numpy.array([x, y])
+
+            diff = controller_coords - mouse_home
+            dist = norm(diff)
+            controller_coords_unit_vec = diff / dist
+
+            if dist < 40:
+                velocity = [0, 0]
+            else:
+                velocity = (0.1*(dist-40)) * controller_coords_unit_vec
+
+            print(f"vel: {velocity}, curr: {x, y}, home: {self.mouse_home_x, self.mouse_home_y}")
+
+            pass
 
     def mouse_click(self):
         if self.mode.get_mode() == "navigation":
@@ -92,4 +119,8 @@ class GestureProcessor:
             self.screen_width, self.screen_height = pyautogui.size()
             self.window_x = 0
             self.window_y = 0
+
+        if cur_mode == "navigation":
+            self.mouse_home_x, self.mouse_home_y = self.last_mouse_pos
+
         print("Mode swapped to", cur_mode)
