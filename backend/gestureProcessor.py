736a69fb1c2f157e83b3c900bf36e8eff9e6d30f
beakae <kantorskibrandon1@gmail.com>
2024-09-15 00:01:27 -0500

    merge-conflict

--- backend/gestureProcessor.py
+++ backend/gestureProcessor.py
@@ -1,15 +1,12 @@
 import mouse
 import numpy
 import pyautogui
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QCursor
 from backend.mode import Mode
 import mouse
-from numpy.linalg import norm
+from constants import left_bound_x, right_bound_x, top_bound_y, bottom_bound_y, margin
 
-left_bound_x, right_bound_x = .2, .8
-top_bound_y, bottom_bound_y = .2, .8
-
+velocity_x_scale = .1
+velocity_y_scale = .1
 
 class GestureProcessor:
     def __init__(self, pyqt_gui):
@@ -20,14 +17,14 @@ class GestureProcessor:
         self.hasClosedFor = 0
         self.tilt_ct = 0
         self.has_changed_mode = True
-        self.isMousePressed = False
         self.has_clicked = True
-        self.mouse_home_x = 0.5
-        self.mouse_home_y = 0.5
-        self.last_mouse_pos = (0.5, 0.5)
+        self.pos_x, self.pos_y = 0, 0
 
     def process_gesture(self, hand):
-        self.handle_movement((hand['dampened_x'], hand['dampened_y']))
+        if self.mode.get_mode() == "navigation":
+            self.handle_navigation_movement((hand['velocity_x'], hand['velocity_y']))
+        else:
+            self.handle_keyboard_movement((hand['dampened_x'], hand['dampened_y']))
 
         is_tilted = hand["angle"] > 135 or hand["angle"] < -135
 
@@ -37,6 +34,10 @@ class GestureProcessor:
         if not is_tilted:
             self.tilt_ct = 0
             self.has_changed_mode = False
+
+        if self.tilt_ct == 5 and not self.has_changed_mode:
+            self.swap_mode()
+            self.has_changed_mode = True
 
         is_closed = not hand['is_open']
 
@@ -52,61 +53,35 @@ class GestureProcessor:
         else:
             if hand['is_open']:
                 self.hasClosedFor = 0
-                if self.isMousePressed:
-                    mouse.release('left')
-                    self.isMousePressed = False
             else:
                 self.hasClosedFor += 1
-
-        if self.tilt_ct == 5 and not self.has_changed_mode:
-            self.swap_mode()
-            self.has_changed_mode = True
 
     def reset_hand_close(self):
         self.hasClosedFor = 0
         self.has_clicked = True
 
-    def handle_movement(self, position):
+    def handle_navigation_movement(self, velocity):
+        self.pos_x += velocity[0] * velocity_x_scale
+        self.pos_y += velocity[1] * velocity_y_scale
+
+        self.pos_x = max(0, min(self.pos_x, self.screen_width))
+        self.pos_y = max(0, min(self.pos_y, self.screen_height))
+
+        mouse.move(self.pos_x, self.pos_y)
+
+    def handle_keyboard_movement(self, position):
         x = (position[0] - left_bound_x) / (right_bound_x - left_bound_x) * self.screen_width
         y = (position[1] - top_bound_y) / (bottom_bound_y - top_bound_y) * self.screen_height
 
         self.last_mouse_pos = x, y
 
-        margin = 10
-        if self.mode.get_mode() == "keyboard" and self.pyqt_gui:
-            x = max(self.window_x + margin, min(x, self.window_x + self.screen_width) - margin)
-            y = max(self.window_y + margin, min(y + self.window_y, self.window_y + self.screen_height) - margin)
-            mouse.move(x, y)
-
-        if self.mode.get_mode() == "navigation":
-            # x, y -> controller coords
-            # self.mouse_home_x, self.mouse_home_y -> mouse home
-            # output: mouse velocity vector
-
-            mouse_home = numpy.array([self.mouse_home_x, self.mouse_home_y], dtype=float)
-            controller_coords = numpy.array([x, y])
-
-            diff = controller_coords - mouse_home
-            dist = norm(diff)
-            controller_coords_unit_vec = diff / dist
-
-            if dist < 40:
-                velocity = [0, 0]
-            else:
-                velocity = (0.1*(dist-40)) * controller_coords_unit_vec
-
-            print(f"vel: {velocity}, curr: {x, y}, home: {self.mouse_home_x, self.mouse_home_y}")
-
-            pass
+        x = max(self.window_x + margin, min(x, self.window_x + self.screen_width) - margin)
+        y = max(self.window_y + margin, min(y, self.window_y + self.screen_height) - margin)
+        
+        mouse.move(x, y)
 
     def mouse_click(self):
-        if self.mode.get_mode() == "navigation":
-            mouse.click('left')
-        elif self.mode.get_mode() == "keyboard":
-            mouse.click('left')
-        else:
-            mouse.press('left')
-            self.isMousePressed = True
+        mouse.click('left')
 
     def swap_mode(self):
         cur_mode = self.mode.swap_mode()
--- constants.py
+++ constants.py
@@ -2,3 +2,8 @@ FRAME_RATE = 30
 global_gui_scale = 0.8
 draw_hands = True
 print_hands = False
+
+# Constants for gestureProcessor.py
+left_bound_x, right_bound_x = .2, .8
+top_bound_y, bottom_bound_y = .3, .7
+margin = 10
